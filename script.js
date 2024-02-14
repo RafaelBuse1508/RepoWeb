051f829d28a1af49acc7b87d5e2f8916b73ac2a2
Rafael Eduardo Buse Bone <75323404+RafaelBuse1508@users.noreply.github.com>
2024-02-14 12:22:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-$(document).ready(function(){
- 
+const buttonNo = document.querySelector('#no')
+const buttonYes = document.querySelector('#yes')
 
-  $('.valentines-day').click(function(){
-      // Animación de desvanecimiento de los elementos del sobre
-      $('.envelope').css({'animation':'fall 3s linear 1', '-webkit-animation':'fall 3s linear 1'});
-      $('.envelope').fadeOut(800, function() {
-          // Ocultar elementos dentro de .valentines-day
-          $('.valentines-day .heart, .valentines-day .text, .valentines-day .front').hide();
+let fontSize = 2
 
-          // Hacer visible la carta con una animación ondulante
-          $('#card').css({'visibility':'visible', 'opacity': 0, 'transform': 'scale(0.1)'});
-          $('#card').animate({'opacity': 1}, {duration: 1000, step: function(now, fx) {
-              var scale = 1 + Math.sin(now * Math.PI) * 0.1; // Calculamos la escala basada en la función seno
-              $(this).css('transform', 'scale(' + scale + ')');
-          }}); // Animación de ondulación
-      });
-  });
-});
+let messages = [
+  'Estas segurx?',
+  'Piensalo bien',
+  'Piensalo muy bien',
+  'Piensalo',
+  'Mira el otro botón'
+]
 
-    (function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
-      .push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
-      n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
-      (window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
-      ml('account', '736768');
+buttonNo.addEventListener('click', () => {
+  fontSize = fontSize + .5
+  buttonYes.style.fontSize = `${fontSize}rem`
+
+  const indexRandom = Math.floor(Math.random() * messages.length)
+
+  buttonNo.textContent = messages[indexRandom]
+})
+
+buttonYes.addEventListener('click', () => {
+  document.querySelector('#message').style.display = 'flex'
+})
